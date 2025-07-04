42984c5e3da2d765800dca0029938112e5d017e6
52base <nnalyschev@icloud.com>
2025-07-04 17:52:30 +0300

    second commit

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 from aiogram import Dispatcher,Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from dotenv import load_dotenv
 
-admins = [6328214493]
-token = "7771933584:AAElPcxJVXlo1MFlw-qt_Yj8BVHrh0-DIP8"
+load_dotenv()
+admins = os.getenv("ADMIN")
+token = os.getenv("TOKEN")
 
 dp = Dispatcher()
 bot = Bot(token=token,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
