727b3a8b8a811fd33c9b1efdf66d49bfbf2061b5
52base <nnalyschev@icloud.com>
2025-07-04 18:13:08 +0300

    six commit

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import asyncio
 from use import admin,user
 from config import dp,bot
+from flask import Flask
+import threading
+import asyncio
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+
+def keep_alive():
+    server = threading.Thread(target=run)
+    server.start()
+
 
 async def main():
 
@@ -9,6 +30,6 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
+    keep_alive()
     asyncio.run(main())
